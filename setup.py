b4c9b221ddc85dba00b9bd7984f6b171c3004d9d
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-02-14 13:24:55 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jdksavdecc',
+      version='1.0',
+      description='JDKS Avdecc Protocol Library',
+      author='Jeff Koftinoff',
+      author_email='jeffk@jdkoftinoff.com',
+      url='http://jdksavdecc-python.statusbar.com/',
+      packages=['jdksavdecc'],
+     )
